f9682f36b45752568d96f0bfabe694da515605f1
Pretty Pandey <prettypandeypearl@gmail.com>
2023-12-12 21:48:32 +0200

    Loading content function and final styling

--- json-display-app/src/App.js
+++ json-display-app/src/App.js
@@ -1,9 +1,23 @@
 import Timeline from './components/Timeline';
+import logo from './assets/logo.jpg';
+import './App.scss'
 
 function App() {
   return (
     <div className="App">
-      <Timeline />
+      <header>
+        <img className='welcome-img' src={logo} alt='Logo' />
+        <div>
+          <h1>Welcome Home</h1>
+          <hr />
+          <h3>Governance, Communion, Services, Teachings, and Training.</h3>
+        </div>
+        
+      </header>
+      <main>
+        <Timeline />
+      </main>
+      
     </div>
   );
 }
--- json-display-app/src/components/Timeline.js
+++ json-display-app/src/components/Timeline.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
+import defaultImg from '../assets/StatementIcon.jpg';
 import './Timeline.scss';
 
 const Timeline = () => {
     const [timelineData, setTimelineData] = useState([]);
     const [itemsToShow, setItemsToShow] = useState(10);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -16,6 +18,7 @@ const Timeline = () => {
                 console.log('Fetched Data:', timelineData);
 
                 setTimelineData(data.timelineData || []);
+                setIsLoading(false);
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
@@ -23,37 +26,49 @@ const Timeline = () => {
 
         fetchData();
     }, []);
-
+    
+    //Load more content function
     const handleLoadMore = () => {
         setItemsToShow(itemsToShow + 10);
     }
 
     return (
         <div className='container'>
-            <ul className='timeline'>
+            {isLoading ? (
+                <p className='loading-text'>Loading content ...</p>
+            ): (
+                <div>
+                <ul className='timeline'>
                 {timelineData.slice(0, itemsToShow).map((item) => (
-                <li key={item.Id}>
-                    <div className='intro'>
-                        <p>{item.Title}</p>
-                        <p><small>{item.Category}</small></p>
-                        <p>{item.Description}</p>
-                        <p><small>{item.CreateDate}</small></p>
-                    </div>
-                    <div className='image-container'>
-                        <img className='image' src={`https://arthurfrost.qflo.co.za/${item.Image}`} alt={item.Title}/>
-                    </div>
+                    <li key={item.Id}>
+                        <div className='intro'>
+                            <p>{item.Title.slice(0, 50)}</p>
+                            <p><small>{item.Category}</small></p>
+                            <p>{item.Description}</p>
+                            <p><small>{item.CreateDate}</small></p>
+                        </div>
+                        <div className='image-container'>
+                            {item.Image ? (
+                                <img className='image' src={`https://arthurfrost.qflo.co.za/${item.Image}`} alt={item.Title}/>
+                            ) : (
+                                <img className='image' src={defaultImg} alt='default' />
+                            )}
+                        </div>
 
-                    <audio controls>
-                    <source src={`https://arthurfrost.qflo.co.za/${item.Audio}`} type="audio/mp3" />
-                    Your browser does not support the audio element.
-                    </audio>
-  
-                </li>
+                        <audio controls>
+                        <source src={`https://arthurfrost.qflo.co.za/${item.Audio}`} type="audio/mp3" />
+                        Your browser does not support the audio element.
+                        </audio>
+    
+                    </li>
                 ))}
-            </ul>
-            {timelineData.length > itemsToShow && (
-                <button className='load-more-btn' onClick={handleLoadMore}>Load More</button>
+                </ul>
+                {timelineData.length > itemsToShow && (
+                    <button className='load-more-btn' onClick={handleLoadMore}>Load More</button>
+                )}
+                </div>
             )}
+            
         </div>
     )
 }
